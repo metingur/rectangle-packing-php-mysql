0261045112fe7eb28a3f988a9a38ebb2e7a7cc15
Metin Gür <metingur@users.noreply.github.com>
2014-01-09 04:02:28 +0200

    Update index3.php

--- index3.php
+++ index3.php
@@ -4,120 +4,12 @@
 	<title>Diktörtgen parçala</title>
 	<meta charset="utf-8">
 <style type="text/css" media="screen">
-	img {
-			
-			background: #3F4C6B;
-
-		}
-	body {
-        font-family: 'Segoe ui', 'Lucida Grande', Tahoma, Helvetica, Arial;
-        padding: 0;
-    }
-
-    h1 {
-        margin: 0.2em 0;
-        padding: 0.2em;
-        color: #F9F7ED;
-        background: #3F4C6B;
-        border-top: 2px solid #ccc;
-        border-bottom: 2px solid #ccc;
-    }
-
-    #canvasContainer {
-        float: left;
-        width: 645px;
-        height: 510px;
-        text-align: center;
-        border: 1px solid #ddd;
-        background: #eee;
-    }
     #canvas {
         background: none repeat scroll 0 0 #C79810;
         text-align: left;
         margin: 4px auto;
         border: 2px solid white;
 		
-    }
-
-    #controls {
-        margin-left: 650px;
-
-        width: 12em !important;
-        width: auto;
-        border: 1px solid #ddd;
-        background: #eee;
-        padding: 1em;
-    }
-
-    #controls label {
-        display: block;
-    }
-    #controls input, #controls select {
-        margin-left: 2em;
-    }
-
-
-    #controls h3.nofit {
-        font-size: 12pt;
-        margin-bottom: 0;
-        background: #ddd;
-        width: 12em;
-        border: 1px solid #888;
-        border-color: #ccc #888 transparent #ccc;
-    }
-    #nofit {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        width: 12em;
-        height: 120px;
-        overflow: scroll;
-        border: 1px solid #888;
-        border-color: #ccc #888 #888 #ccc;
-    }
-    #nofit li {
-        background: white;
-    }
-    #nofit li:hover {
-        background: #eee;
-    }
-
-    #status {
-        font-size: 12pt;
-        height: 1.5em;
-        line-height: 1.5em;
-        background: #444;
-
-        color: #eee;
-    }
-
-    #status a.refresh {
-        float: right;
-        height: 1.5em;
-        line-height: 1.5em;
-
-        text-decoration: none;
-        color: white;
-        background: #888;
-        padding: 0 3px;
-        border-left: 1px solid #eee;
-    }
-    #status a.refresh:hover {
-        background: white;
-        color: #888;
-    }
-
-    span.label {
-        color: #ccc;
-    }
-
-    #footer {
-        text-align: center;
-        border-top: 2px solid #ccc;
-        border-bottom: 2px solid #ccc;
-        background: #3F4C6B;
-        color: white;
-        margin-top: 1em;
     }
 	</style>
 </head>
