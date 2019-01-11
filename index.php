2e61acc5e578b15388847bd1a601b88e67baedba
Metin Gür <metingur@users.noreply.github.com>
2019-01-11 16:34:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,84 +1,115 @@
-<div align="center" style="border:1px">
-<?php
-$conn = mysqli_connect("localhost","db_user","password","db_name");
- 
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <title>Rectangle Packing</title>
+   </head>
+   <body>
+      <div class="container">
+         <br>
+         <div class="row">
+            <div class="col-sm" align="center">
+               <div class="alert alert-primary" role="alert">
+                  Creat a new project
+               </div>
+               <form method='POST' action='result.php'>
+                  <div class="input-group mb-3">
+                     <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Project name</span>
+                     </div>
+                     <input type="text" name="name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required autofocus>
+                  </div>
+                  <div class="card">
+                     <div class="card-header">Enter the height and width of the rectangle</div>
+                     <div class="card-body">
+                        <div class="row">
+                           <div class="col">
+                              <input type="text" name="yuk" class="form-control" placeholder="Height" required autofocus>
+                           </div>
+                           <div class="col">
+                              <input type="text" name="gen" class="form-control" placeholder="Width" required autofocus>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <br>
+                  <div class="card">
+                     <div class="card-header">Enter the desired size (H x W)</div>
+                     <div class="card-body">
+                        <div class="input-group control-group after-add-more">
+                           <input type="text" name="boyut[]" class="form-control" placeholder="Height"  required autofocus>
+                           <div class="input-group-btn"></div>
+                           <input type="text" name="boyut[]" class="form-control" placeholder="Width"  required autofocus>
+                           <div class="input-group-btn">
+                              <button class="btn btn-success add-more" type="button"><i class="glyphicon glyphicon-plus"></i> &#xA0;Add &#xA0; &#xA0; &#xA0;</button>
+                           </div>
+                        </div>
+                        <br>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                     </div>
+                  </div>
+               </form>
+               <!-- Copy Fields -->
+               <div class="copy d-none">
+                  <div class="control-group input-group" style="margin-top:10px">
+                     <input type="text" name="boyut[]" class="form-control" placeholder="Height" required autofocus>
+                     <div class="input-group-btn"></div>
+                     <input type="text" name="boyut[]" class="form-control" placeholder="Width" required autofocus>
+                     <div class="input-group-btn">
+                        <button class="btn btn-danger remove" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm" align="center">
+               <?php
+                  require_once("database.php");
+                              $result = mysqli_query($conn,"SELECT  `name` FROM `table`");
+                           ?>
+               <div class="alert alert-primary" role="alert">
+                  Open a saved project
+               </div>
+               <form method="post" name="city" action="getfromdb.php">
+                  <div class="input-group">
+                     <select class="custom-select" name="name" id="inputGroupSelect04" aria-label="Example select with button addon">
+                        <option selected>Choose...</option>
+                        <?php
+                           foreach ($result as $row) {
+                           $name = $row["name"];
+                           ?>
+                        <option value="<?php echo $name ?>">
+                           <?php echo  $name ?>
+                        </option>
+                        <?php
+                           }?>
+                     </select>
+                     <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit">Submit</button>
+                     </div>
+                  </div>
 
-if (mysqli_connect_errno()) {
-    echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
-    exit();
-}
-$result = mysqli_query($conn,"SELECT  `name` FROM `table`");
-
-?>
-Open Saved Projects.
-</br>
-<form method="post" name="city" action="index3.php">
-<select name="name" >
-<option value="0" selected="selected"> Choose a project.</option>
-<?php
-foreach ($result as $row) {
-
-$name = $row["name"];
-
-?>
-<option value="<?php echo $name ?>"> <?php echo  $name ?> </option>
-<?php
-}?>
-</select>
-<input type='submit' value='Submit' name='submit'>
-</form>
---------------------------------------------
-</br>
-Creat a new project.
-
-<form method='POST' action='index2.php'>
-<p>Project name: <input type="text" name="name" /></p>
-
-<p> Mdf boyutu 170 x 210 - 183 x 366 - 210 x 244 - 210 x 280 </p>
-<p> Sunta boyutu 170 x 210 -	183 x 366 -	210 x 244 -	210 x 280 -	210 x 366 -	220 x 280 -	220 x 366 </p>
-<p> Enter the height and width of the sheet: </p>
-<p>Height: <input type="text" name="yuk" /></p>
-<p>Width: <input type="text" name="gen" /></p>
-</br>
-
-
-
-<p>Enter the desired size (H x W):</p>
-<input type="text" name="boyut[0][w]" >
-<input type="text" name="boyut[0][h]" >
-</br>
-<input type="text" name="boyut[1][w]" >
-<input type="text" name="boyut[1][h]" >
-</br>
-<input type="text" name="boyut[2][w]" >
-<input type="text" name="boyut[2][h]" >
-</br>
-<input type="text" name="boyut[3][w]" >
-<input type="text" name="boyut[3][h]" >
-</br>
-<input type="text" name="boyut[4][w]" >
-<input type="text" name="boyut[4][h]" >
-</br>
-<input type="text" name="boyut[5][w]" >
-<input type="text" name="boyut[5][h]" >
-</br>
-<input type="text" name="boyut[6][w]" >
-<input type="text" name="boyut[6][h]" >
-</br>
-<input type="text" name="boyut[7][w]" >
-<input type="text" name="boyut[7][h]" >
-</br>
-<input type="text" name="boyut[8][w]" >
-<input type="text" name="boyut[8][h]" >
-</br>
-<input type="text" name="boyut[9][w]" >
-<input type="text" name="boyut[9][h]" >
-</br>
-<input type="text" name="boyut[10][w]" >
-<input type="text" name="boyut[10][h]" >
-</br>
-
-<input type='submit' value='Submit' name='submit'>
-
-</form>
-</div>
+				</form>
+            </div>
+         </div>
+      </div>
+      <script type="text/javascript">
+         $(document).ready(function() {
+         
+             $(".add-more").click(function() {
+                 var html = $(".copy").html();
+                 $(".after-add-more").after(html);
+             });
+         
+             $("body").on("click", ".remove", function() {
+                 $(this).parents(".control-group").remove();
+             });
+         
+         });
+      </script>
+   </body>
+</html>
